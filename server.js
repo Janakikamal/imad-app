aa89e5d59bbed14f605baacf892b30b428207c07
Janakikamal <30664796+Janakikamal@users.noreply.github.com>
2017-08-20 17:39:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,15 +12,6 @@ app.get('/', function (req, res) {
 
 
 
-app.get('/article-two',function(req,res){
-    res.send('árticle two is requested and will be served');
-});
-
-app.get('/article-three',function(req,res){
-    res.send('árticle three is requested and will be served');
-});
-
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
