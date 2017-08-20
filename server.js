8022501d5e929cc9412a07b2a554b3a39d79f5f1
Janakikamal <30664796+Janakikamal@users.noreply.github.com>
2017-08-20 18:43:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var crypto=require('çrypto');
 
 var app = express();
 app.use(morgan('combined'));
@@ -12,14 +12,6 @@ app.get('/', function (req, res) {
 
 
 
-app.get('/article-two',function(req,res){
-  res.sendFile(path.join(__dirname, 'ui', 'Article-One.html'));
-});
-
-app.get('/article-three',function(req,res){
-    res.send('árticle three is requested and will be served');
-});
-
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
@@ -29,6 +21,17 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+function hash(input,salt)
+{
+    var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return hashed.tostring('hex');
+}
+
+app.get('/hash/:input',function(req,res)
+{
+    var hashedstring=hash(req.params.input,'this is some random string');
+    res.send(hashedstring);
+});
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
