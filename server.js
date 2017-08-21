db588b420fe036482e91659672324b8c4405e070
Janakikamal <30664796+Janakikamal@users.noreply.github.com>
2017-08-21 22:17:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,41 +1,40 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var crypto = require('çrypto');
 
 var app = express();
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 
+app.get('/article-one', function (req, res) {
+   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+});
 
+
+app.get('/article-two', function (req, res) {
+    res.send('Article two requested and will be served');
+});
+
+app.get('/article-three', function (req, res) {
+    res.send('Article three requested and will be served');
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  res.sendFile(path.join(__dirname, 'ui', '/madi.png'));
 });
-
-function hash(input,salt)
-{
-    var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
-    return hashed.tostring('hex');
-}
-
-app.get('/hash/:input',function(req,res)
-{
-    var hashedstring=hash(req.params.input,'this is some random string');
-    res.send(hashedstring);
-});
-
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
+
+
 
 var port = 80;
 app.listen(port, function () {
