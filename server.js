7fb0b86858d4ab33cd6950a04510982f0bc23016
Janakikamal <30664796+Janakikamal@users.noreply.github.com>
2017-08-21 11:09:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var crypto = require('çrypto')
 
 var app = express();
 app.use(morgan('combined'));
@@ -19,6 +19,18 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+function hash(input,salt)
+{
+    var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return hashed.tostring('hex');
+}
+
+app.get('/hash/:input',function(req,res)
+{
+    var hashedstring=hash(req.params.input,'this is some random string');
+    res.send(hashedstring);
 });
 
 
